d459ea63bddd3e49f9b964f9985a1f687bbfaa98
Vaibhav Patravali <vaibhavpatravali007@gmail.com>
2019-11-13 18:48:01 +0530

    Changes done in header footer  loginpage & cards

--- app/src/app/config/declarations.ts
+++ app/src/app/config/declarations.ts
@@ -14,6 +14,8 @@ window['neutrinos'] = {
 }
 
 //CORE_REFERENCE_IMPORTS
+//CORE_REFERENCE_IMPORT-cardComponent
+import { cardComponent } from '../components/cardComponent/card.component';
 //CORE_REFERENCE_IMPORT-footerComponent
 import { footerComponent } from '../components/footerComponent/footer.component';
 //CORE_REFERENCE_IMPORT-headerComponent
@@ -99,6 +101,8 @@ export const appDeclarations = [
   NMapComponent,
   ArtImgSrcDirective,
   //CORE_REFERENCE_PUSH_TO_DEC_ARRAY
+//CORE_REFERENCE_PUSH_TO_DEC_ARRAY-cardComponent
+cardComponent,
 //CORE_REFERENCE_PUSH_TO_DEC_ARRAY-footerComponent
 footerComponent,
 //CORE_REFERENCE_PUSH_TO_DEC_ARRAY-headerComponent
@@ -178,5 +182,5 @@ export const appRoutes = [{path: 'instructorregistration', component: instructor
 children: [{path: 'instructors', component: instructorsComponent},{path: 'trainee', component: traineeComponent},{path: 'courses', component: coursesComponent},{path: 'dashboardcontent', component: dashboardcontentComponent}]},{path: 'instructordashboard', component: instructordashboardComponent,
 children: [{path: 'addcourse', component: addcourseComponent,
 children: []},{path: 'instructorDashboardContent', component: instructordashboradcontentComponent},{path: 'courseList', component: courselistComponent}]},{path: 'traineedashboard', component: traineedashboardComponent,
-children: [{path: 'traineedashboardcontent', component: traineedashboardcontentComponent},{path: 'selfjoinedcourses', component: selfjoinedcoursesComponent}]},{path: 'login', component: loginformComponent},{path: 'contactus', component: contactusComponent},{path: 'product', component: productComponent},{path: 'home', component: homeComponent},{path: 'header', component: headerComponent},{path: 'footer', component: footerComponent},{path: '', redirectTo: '/home', pathMatch: 'full'},{path: '**', component: PageNotFoundComponent}]
+children: [{path: 'traineedashboardcontent', component: traineedashboardcontentComponent},{path: 'selfjoinedcourses', component: selfjoinedcoursesComponent}]},{path: 'login', component: loginformComponent},{path: 'contactus', component: contactusComponent},{path: 'product', component: productComponent},{path: 'home', component: homeComponent},{path: 'header', component: headerComponent},{path: 'footer', component: footerComponent},{path: 'card', component: cardComponent},{path: '', redirectTo: '/home', pathMatch: 'full'},{path: '**', component: PageNotFoundComponent}]
 // CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY_END
